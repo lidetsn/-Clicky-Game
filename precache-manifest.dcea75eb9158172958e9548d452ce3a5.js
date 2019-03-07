aad8bd3da908eb3360b3e770ac4063bec3c737f9
Lidet s <luk4ls@yahoo.com>
2019-03-06 17:06:27 -0800

    Updates

---
+++ precache-manifest.dcea75eb9158172958e9548d452ce3a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "0fbdc8e3e32eff92ea79",
+    "url": "/Clicky-Game/static/js/main.0fbdc8e3.chunk.js"
+  },
+  {
+    "revision": "62507dd0e61ef6a60d15",
+    "url": "/Clicky-Game/static/js/2.62507dd0.chunk.js"
+  },
+  {
+    "revision": "0fbdc8e3e32eff92ea79",
+    "url": "/Clicky-Game/static/css/main.0761c117.chunk.css"
+  },
+  {
+    "revision": "a5b0072ec3a111e8f682eb63b6537e38",
+    "url": "/Clicky-Game/index.html"
+  }
+];
